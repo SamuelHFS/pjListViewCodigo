f17b81c293c3d3204b7b36efab2bb39d1b3708d3
SamuelHFS <samuel.jpg1@gmail.com>
2021-10-04 08:53:09 -0300

    novo projetinho times de futebol

---
+++ ListViewCodigo/ListViewCodigo/ListViewCodigo/model/Times.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewCodigo.model
+{
+    class Times
+    {
+        public string nome { get; set; }
+
+        public string imagem { get; set; }
+
+        public string descricao { get; set; }
+
+        public string tecnico { get; set; }
+
+        public ArraySegment<string> jogadores { get; set; }
+
+    }
+}
