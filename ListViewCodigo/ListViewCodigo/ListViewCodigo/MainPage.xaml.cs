b373d05a3ec6f19af82467f0a9fdf3c537cb5d82
SamuelHFS <samuel.jpg1@gmail.com>
2021-10-04 22:36:11 -0300

    funcionando :thumbsup:

--- ListViewCodigo/ListViewCodigo/ListViewCodigo/MainPage.xaml.cs
+++ ListViewCodigo/ListViewCodigo/ListViewCodigo/MainPage.xaml.cs
@@ -24,33 +24,33 @@ namespace ListViewCodigo
             
                 new Times{
                 nome = "Galudo",
-                imagem = "img/galudo.png",
-                descricao = "maior de minas, CONFIA",
-                tecnico = "cabelo de boneca",
+                imagem = "galudo.png",
+                descricao = "Maior de minas, CONFIA",
+                tecnico = "Cuca cabelo de boneca",
                 jogadores = new string[] {"Everson","Arana","Nathan S", "Alonso", "Mariano", "Allan", "Jair", "Nacho", "Savarino", "Hulk", "Keno"}
                 },
 
                 new Times{
                 nome = "Palmares",
-                imagem = "img/palmeiras.png",
-                descricao = "nao tem copinha e nao tem mundial",
-                tecnico = "abelaum",
+                imagem = "palmeiras.png",
+                descricao = "Não tem copinha e não tem mundial",
+                tecnico = "Abelaum",
                 jogadores = new string[] {"Weverton", "Gustavo Gomez", "Luan", "Piquerez", "Marcos Rocha", "Danilo", "Zé Rafael", "Scarpa", "Wesley", "Luiz Adriano", "Rony Rústico" }
                 },
 
                 new Times{
                 nome = "Flamilicia",
-                imagem = "img/flamengo.png",
-                descricao = "sem meu josé alberto wright eu não consigo",
-                tecnico = "renight",
+                imagem = "flamengo.png",
+                descricao = "Sem meu José Alberto Wright eu não consigo",
+                tecnico = "Renight",
                 jogadores = new string[] {"Diego", "Leo Pereira", "Rodrigo Caio", "Isla", "Filipe Luís", "Arão", "Andreas Pereira", "Everton Ribeiro", "Arrascaxeira", "Belo Horizonte", "Gabigordo" }
                 },
 
                 new Times{
                 nome = "Lion do Pici",
-                imagem = "img/fortaleza.png",
-                descricao = "quem não tem história conta a dos outros",
-                tecnico = "vojfoda",
+                imagem = "fortaleza.png",
+                descricao = "Quem não tem história conta a dos outros",
+                tecnico = "Vojfoda",
                 jogadores = new string[] {"Filipe Alves", "Tinga", "Benevenuto", "Titi", "Bruno Melo", "Ederson", "Felipe", "Lucas Lesma", "Yago Pikachu", "W.Paulista", "David" }
                 }
 
@@ -62,7 +62,7 @@ namespace ListViewCodigo
             lstview.ItemTemplate.SetBinding(ImageCell.TextProperty, "nome");
             lstview.ItemTemplate.SetBinding(ImageCell.DetailProperty, "tecnico");
             
-            this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+           
 
             lstview.ItemSelected += Lstview_ItemSelected;
             lstview.ItemTapped += Lstview_ItemTapped;
@@ -77,7 +77,7 @@ namespace ListViewCodigo
 
         private void Lstview_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            var time = e.SelectedItem as Time;
+            var time = e.SelectedItem as Times;
             Navigation.PushAsync(new Apresentacao(time));
         }
     }
